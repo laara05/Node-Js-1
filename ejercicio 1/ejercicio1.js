3181ff72ad421e515b9561d814b90159db93de08
laara05 <laraniza91@gmail.com>
2024-03-16 19:40:13 -0300

    first commit

---
+++ ejercicio 1/ejercicio1.js
@@ -0,0 +1,2 @@
+console.log('Este es mi primer programa con Node.js')
+console.log('Fin')
---
+++ ejercicio 2/ejercicio2 corregido.js
@@ -0,0 +1,11 @@
+const fs = require('node:fs')
+function leer(error, datos) {
+    if (error)
+      console.log(error)
+    else
+      console.log(datos.toString())
+  }
+  
+  fs.readFile('./archivo1.txt', leer)
+  
+  console.log('última línea del programa')
